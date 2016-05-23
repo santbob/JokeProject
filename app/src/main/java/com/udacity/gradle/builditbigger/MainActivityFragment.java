905a7a72113cf4f10f1d22d434def4e62db0233b
Santhosh Nageshwar <santhoshbabun@gmail.com>
2016-05-22 21:44:44 -0700

    commiting first two steps

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,55 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.androidjokelibrary.JokeActivity;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        Button tellAJoke = (Button) root.findViewById(R.id.tell_a_joke);
+        tellAJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchDispalyJokeIntent();
+            }
+        });
+
+        return root;
+    }
+
+    private void launchDispalyJokeIntent() {
+        Intent intent = new Intent(getActivity(), JokeActivity.class);
+        JokeLibrary jl = new JokeLibrary();
+        intent.putExtra(JokeActivity.JOKE_KEY, jl.getJoke());
+        startActivity(intent);
+    }
+}
---
+++ jokelibrary/src/main/java/com/udacity/gradle/builditbigger/JokeLibrary.java
@@ -0,0 +1,8 @@
+package com.udacity.gradle.builditbigger;
+
+public class JokeLibrary {
+
+    public String getJoke(){
+        return "This is totally a funny joke";
+    }
+}
